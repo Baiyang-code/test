d7798eb0a12a4eadf8fd5932960cbb8e4dcfda4b
Baiyang-code <1261336921@qq.com>
2021-02-04 04:11:32 +0800

    提交代码来测试

---
+++ 源.c
@@ -0,0 +1,18 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	/*int a = 10;
+	char* p = (char*)&a;
+	if (*p == 10)
+		printf("Ð¡¶Ë\n");
+	else
+		printf("´ó¶Ë\n");*/
+
+
+	int i = -20;
+	unsigned int j = 10;
+	printf("%u\n", -1);
+	printf("%u", -2);
+	return 0;
+}
